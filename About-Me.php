5a93817ed2391e5abe0a7249099792a0d9a5fd66
David John <david.john@mail.utoronto.ca>
2021-05-20 15:23:58 -0400

    Completed About-Me.php

---
+++ About-Me.php
@@ -0,0 +1,40 @@
+<!--
+
+All images used were taken from the following website and placed in the public domain:
+
+https://www.flickr.com/photos/48660975@N02/19663096295
+"Background" by redcctshirt is marked under CC0 1.0. To view the terms, visit https://creativecommons.org/licenses/cc0/1.0/
+
+
+ -->
+
+
+<?php
+
+  $tabTitle = "About Me";
+  $favicon = "Treefavicon.ico";
+  $pageID= "006-Page";
+  include 'header.php';
+
+  include 'titleBar.php';
+
+  $pageClass = "pg6LgContainer";
+  $quoteHTML = '';
+  include 'lgContainerDiv.php';
+
+  $position = "Shitotsu";
+  $cardTitle = "WHO AM I?";
+  $cardText = 'Just a normal guy moving forward.';
+  include 'revealCard.php';
+
+  $LeftColSideText = "About Me";
+  $RightColSideText = "
+  I am an immigrant to Canada; I came here almost 20 years ago and became a citizen. I went to school at the University of Toronto and graduated from their Electrical Engineering program. I enjoy working on my websites, watching tv and movies, reading books and graphic novels, listening to music and playing the piano.
+  <br />
+  <br />
+  I believe that it is important to never give up and that everything ends when you stop believing in the value of what you offer to the world as well as what it offers to you. In that case, I will never stop striving to make the most of what I have in life.";
+  include 'TwoColSec.php';
+
+  include 'footer.php';
+
+ ?>
---
+++ TwoColSec.php
@@ -0,0 +1,19 @@
+<section class="TwoColSec">
+  <div class="container-fluid">
+    <div class="card-group">
+      <div class="card ColSide">
+        <div class="card-body">
+          <p class="card-text LeftColSide"><?php echo $LeftColSideText ?></p>
+        </div>
+      </div>
+      <div class="card ColSide">
+        <div class="card-body">
+          <p class="card-text RightColSide">
+            <?php echo $RightColSideText ?>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
